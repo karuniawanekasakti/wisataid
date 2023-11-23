64cfbab65d8561157e6da0139dde74c9240e5d17
karuniawanekasakti <karuniawankaka80@mail.ugm.ac.id>
2023-11-23 17:18:07 +0700

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { CssBaseline } from '@mui/material'
+import Header from './components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+    </>
+  )
+}
+
+export default App
