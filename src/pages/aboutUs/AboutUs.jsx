e3fb95207d6d1995286ae23a436b7e8c8871bbef
ridwanemelen <ridwanemelen@gmail.com>
2023-12-16 02:43:43 +0700

    penambahan logic carousel profile pada about us

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -8,7 +8,11 @@ import Grid from '@mui/material/Unstable_Grid2';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Stack from '@mui/material/Stack';
 import MailIcon from '@mui/icons-material/Mail';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import LanguageIcon from '@mui/icons-material/Language';
+import Button from '@mui/material/Button';
+
 import './AboutUs.css';
 
   const teamMembers = [
@@ -17,7 +21,7 @@ import './AboutUs.css';
       name: 'Karuniawan Ekasakti',
       role: 'Front-End Developer',
       university: 'Universitas Gadjah Mada',
-      image: 'https://drive.google.com/uc?id=1wryo7xCdgGdY7Lla7bgV94w8Vts3YbmV',
+      image: 'https://drive.google.com/uc?id=1cHBtuSD7vEsjDBQYYlM8J6Rp8Ok17sXS',
     },
     {
       id: 2,
@@ -51,11 +55,23 @@ import './AboutUs.css';
 
 
 const AboutUs = () => {
+
+  const [activeSlide, setActiveSlide] = React.useState(0);
+
+  const nextSlide = () => {
+    setActiveSlide((prevIndex) => (prevIndex === teamMembers.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const prevSlide = () => {
+    setActiveSlide((prevIndex) => (prevIndex === 0 ? teamMembers.length - 1 : prevIndex - 1));
+  };
+
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Box sx={{ bgcolor: 'transparent', marginTop: '17%', flexGrow: 1}}>
+        <Box sx={{ bgcolor: 'transparent', marginTop: '70px', flexGrow: 1}} className='box-konten1'>
 	       	<Grid container sx={{spacing: '2', minHeight: '160', marginLeft: '5%', marginRight: '5%',}}>
 			        <Grid xs={12}>	       		
 					       	<Typography variant="h4" gutterBottom sx={{textAlign:'left',}} fontWeight="bold">
@@ -75,23 +91,38 @@ const AboutUs = () => {
 					        </Typography>
 				    	</Grid>
 			    </Grid>
-          <Grid container sx={{ spacing: '2', minHeight: '160', marginLeft: '5%', marginRight: '5%', alignItems: 'center', justifyContent: 'center' }}>
-             {teamMembers.map((person) => (
-               <Grid key={person.id} xs={12} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', marginTop: '20px', marginBottom: '20px' }}>
-                 {/* Menggunakan URL dari Google Drive atau Internet */}
-                 <Avatar src={person.image} sx={{ height: '270px', width: '270px', marginBottom: '20px' }} />
-                 <Typography variant="h6" gutterBottom fontWeight="bold">
-                   {person.name}
-                 </Typography>
-                 <Typography variant="body1" gutterBottom>
-                   {person.role}
-                 </Typography>
-                 <Typography variant="caption" gutterBottom>
-                   {person.university}
-                 </Typography>
-               </Grid>
-             ))}
-          </Grid>
+					<Grid container sx={{ spacing: '2', minHeight: '160', marginLeft: '5%', marginRight: '5%', alignItems: 'center', justifyContent: 'center' }}>
+					  <div className="carousel-container">
+					    <div className="carousel-wrapper" style={{ transform: `translateX(-${activeSlide * 100}%)` }}>
+					      {teamMembers.map((person) => (
+					        <Grid key={person.id} xs={12} className="carousel-item" sx={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', marginTop: '20px', marginBottom: '20px', marginLeft: '0.1px' }}>
+					          <Avatar src={person.image} sx={{ height: '250px', width: '250px', marginBottom: '20px' }} />
+					          <Typography variant="h6" gutterBottom fontWeight="bold">
+					            {person.name}
+					          </Typography>
+					          <Typography variant="body1" gutterBottom>
+					            {person.role}
+					          </Typography>
+					          <Typography variant="caption" gutterBottom>
+					            {person.university}
+					          </Typography>
+					        </Grid>
+					      ))}
+					    </div>
+					    <div style={{textAlign: 'center'}} className="two-button">
+					    		<Button>
+										<KeyboardArrowLeft onClick={prevSlide} color="action" style={{fontSize:'3rem'}}  sx={{ '&:hover': { color: 'primary.dark' } }}/>
+					    		</Button>
+									<Button>
+					  			<KeyboardArrowRight onClick={nextSlide} color="action" style={{fontSize:'3rem'}}  sx={{ '&:hover': { color: 'primary.dark' } }}/>										
+									</Button>
+					    </div>
+					  </div>
+					{/*  <KeyboardArrowLeft onClick={prevSlide} color="action" style={{fontSize:'3rem'}}  sx={{ '&:hover': { color: 'primary.dark' } }}/>
+					  <KeyboardArrowRight onClick={nextSlide} color="action" style={{fontSize:'3rem'}}  sx={{ '&:hover': { color: 'primary.dark' } }}/>*/}
+{/*					  <button onClick={prevSlide}>Previous</button>
+					  <button onClick={nextSlide}>Next</button>*/}
+					</Grid>
 	        <Grid container>
 							<Grid>
 								<Box>
@@ -99,7 +130,7 @@ const AboutUs = () => {
 						        <li style={{ marginBottom: '15px' }}>
 						          <Stack spacing={2} direction="row" alignItems="center">
 						            <LocationOnIcon />
-						            <Typography>Komplek Stella No. 78x, Bandung</Typography>
+						            <Typography>Daerah Istimewa Yogyakarta</Typography>
 						          </Stack>
 						        </li>
 						        <li style={{ marginBottom: '15px' }}>
