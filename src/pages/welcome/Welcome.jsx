2a5e1483c68a05bbd0aca84dc0b0a152e4547863
ridwanemelen <ridwanemelen@gmail.com>
2023-12-19 04:14:03 +0700

    responsive welcome

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -3,8 +3,7 @@ import SelectComponent from '../../components/Select/SelectComponent';
 import ButtonComponent from '../../components/Button/ButtonComponent';
 import Stack from '@mui/material/Stack';
 import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
-
-
+import Paper from '@mui/material/Paper';
 import HeroImage from '../../assets/img/background 1.png'
 import './Welcome.css'
 import { useEffect } from 'react';
@@ -43,33 +42,35 @@ const Welcome = () => {
         <div className='bg-hero'>
           <img src={HeroImage} alt="Hero Image" />
           <div className='inner'>
-            <Stack spacing={1} direction={'column'}>
-              <h1 className='sub'>Mau kemana hari ini?</h1>
-              <SelectComponent
-                id="city"
-                value={selectedCity}
-                onChange={(e) => setSelectedCity(e.target.value)}
-                htmlFor="city"
-                variant="outlined"
-                size="medium"
-                label="City"
-                placeholder="Pilih Provinsimu"
-                menuItems={Array.isArray(city) ? city.map((item) => ({
-                  label: item.label,
-                  value: item.value,
-                })) : []}
-              />
-              <div className='button'>
-                <ButtonComponent
-                  text='Cari'
-                  endIcon={<KeyboardArrowRightRoundedIcon fontSize='small' />}
-                  size='medium'
-                  variant='contained'
-                  to={`/list-wisata/${selectedCity}`}
+            <Paper variant="outlined" style={{ borderRadius: '10px', backdropFilter: 'blur(1px)', backgroundColor: 'rgba(0, 0, 0, 0.2)', marginTop: '150px' }}>
+              <Stack spacing={1} direction={'column'} style={{margin: '30px'}}>
+                <h1 className='sub'>Mau kemana hari ini?</h1>
+                <SelectComponent
+                  id="city"
+                  value={selectedCity}
+                  onChange={(e) => setSelectedCity(e.target.value)}
+                  htmlFor="city"
+                  variant="outlined"
+                  size="medium"
+                  label="City"
+                  placeholder="Pilih Provinsimu"
+                  menuItems={Array.isArray(city) ? city.map((item) => ({
+                    label: item.label,
+                    value: item.value,
+                  })) : []}
                 />
-              </div>
-              <a href="/home">Skip</a>
-            </Stack>
+                <div className='button'>
+                  <ButtonComponent
+                    text='Cari'
+                    endIcon={<KeyboardArrowRightRoundedIcon fontSize='small' />}
+                    size='medium'
+                    variant='contained'
+                    to={`/list-wisata/${selectedCity}`}
+                  />
+                </div>
+                <a href="/home">Skip</a>
+              </Stack>
+            </Paper>
           </div>
         </div>
       </div>
